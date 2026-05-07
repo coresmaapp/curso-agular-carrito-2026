7168c76040bc9223686a44d4fb0b2fd78713807b
elder <correo@correo.com>
2026-05-07 11:32:39 -0600

    se agrega proxy- cors

--- src/app/core/service/producto.ts
+++ src/app/core/service/producto.ts
@@ -1,16 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { ApiResponse } from '@modules/productos/models/product.models'
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '@environments/environment'
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductoService {
-  private apiUrl = 'https://softder.com/api/shoppingcart/public-products';
+  private apiUrl = `${environment.apiUrl}/api/shoppingcart/public-product`;
+  // private apiUrl = `/api/shoppingcart/public-products`;
 
-  constructor(private http: HttpClient) {
-  }
+  // constructor(private http: HttpClient) {
+  // }
+
+  private http = inject(HttpClient)
+
+
   public getAllProducts(): Observable<ApiResponse> {
     return this.http.get<ApiResponse>(this.apiUrl)
   }
